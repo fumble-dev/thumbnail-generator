bcaa2f7334e15cb35b7fca2c09e47ea0aea1ae63
udanth <udanth.work@gmail.com>
2026-01-05 15:20:25 +0530

    init

---
+++ client/src/data/features.tsx
@@ -0,0 +1,19 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Analyzes your title, content, and trends to generate thumbnails designed to maximize clicks.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Catchy Designs",
+        description: "Creates visually striking thumbnails with balanced colors, bold text, and high-impact layouts.",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Customize text, colors, images, and layout easily to match your brand and creative style.",
+    }
+];
