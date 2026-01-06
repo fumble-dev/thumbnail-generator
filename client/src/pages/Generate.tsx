f67e0c7b6a6ed6f46a4f8aa515053547ef3d3a46
udanth <udanth.work@gmail.com>
2026-01-06 12:18:37 +0530

    aspect ratio and style selector added

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom'
+import { colorSchemes, type AspectRatio, type IThumbnail, type ThumbnailStyle } from '../assets/assets';
+import AspectRatioSelector from '../components/AspectRatioSelector';
+import StyleSelector from '../components/StyleSelector';
 
 const Generate = () => {
+
+  const {id} = useParams();
+
+  const [title,setTitle] = useState('')
+  const [additionalDetails, setAdditionalDetails] = useState('')
+
+  const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
+  const [loading, setLoading] = useState(false)
+
+  const [aspectRatio, setAspectRatio] = useState<AspectRatio>('16:9')
+  const [colorSchemeId, setColorSchemeId] = useState<string>(colorSchemes[0].id)
+  const [style, setStyle] = useState<ThumbnailStyle>('Bold & Graphic')
+
+  const [styleDropdownOpen, setStyleDropdownOpen] = useState(false)
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="pt-24 min-h-screen">
+        <main className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8'>
+          <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+            {/* left */}
+            <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+                
+                <div className="">
+                  <h2 className="text-xl font-bold text-zinc-100">Create Your Thumbnail</h2>
+                  <p className="text-sm text-zinc-400">Describe your vision and let AI bring it to life.</p>
+                </div>
+
+                <div className="space-y-5">
+
+                  {/* title input */}
+                  <div className="space-y-2">
+                    <label htmlFor="" className='block text-sm font-medium'>Title or Topic</label>
+                    <input type="text" value={title} 
+                          onChange={(e)=>setTitle(e.target.value)} 
+                          maxLength={100} 
+                          placeholder='e.g,. 10 Tips For Better Sleep' 
+                          className='w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500'
+                    />
+
+                    <div className="flex justify-end">
+                      <span className='text-xs text-zinc-400'>{title.length}/100</span>
+                    </div>
+
+                  </div>
+
+                  {/* aspect ratio */}
+                  <AspectRatioSelector value={aspectRatio} onChange={setAspectRatio}  />
+                  {/* style */}
+                  <StyleSelector value={style} onChange={setStyle} isOpen={styleDropdownOpen} setIsOpen={setStyleDropdownOpen} />
+                  {/* color scheme */}
+
+                  {/* details */}
+                  <div className="space-y-2">
+                    <label className='block text-sm font-medium'>Additional Prompts <span className='text-xs text-zinc-400'>(Optional)</span></label>
+                    <textarea value={additionalDetails} onChange={(e)=>setAdditionalDetails(e.target.value)} rows={3} placeholder='Add any specific elements, mood, or style preferences..' className='w-full py-3 px-4 rounded-lg border border-white/10 bg-white/6 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none' />
+                  </div>
+
+                </div>
+
+                {/* button */}
+                {!id && (
+                  <button className='text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors'>
+                    {loading ? "Generating ":"Generate Thumbnail"}
+                  </button>
+                )}
+              </div>
+            </div>
+            {/* right */}
+            <div className=""></div>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
